c23b972cf771c5b932d43c8090a06a34a84cd3f5
Kevin Coleman <kcaws@amazon.com>
2022-01-14 13:36:02 -0800

    Updating

--- lib/addons/aws-for-fluent-bit/index.ts
+++ lib/addons/aws-for-fluent-bit/index.ts
@@ -25,38 +25,41 @@ const defaultProps: AwsForFluentBitAddOnProps = {
     repository: 'https://aws.github.io/eks-charts',
     namespace: 'kube-system',
     values: {}
-}
+};
 
 /**
  * AwsForFluentBitAddOn deploys FluentBit into an EKS cluster using the `aws-for-fluent-bit` Helm chart.
  * https://github.com/aws/eks-charts/tree/master/stable/aws-for-fluent-bit
+ * 
+ * For information on how to configure the `aws-for-fluent-bit` Helm chart to forward logs and metrics to AWS services like CloudWatch or Kinesis, please view the values.yaml spec provided by the chart.
+ * https://github.com/aws/eks-charts/blob/master/stable/aws-for-fluent-bit/values.yaml#L54
  */
 export class AwsForFluentBitAddOn extends HelmAddOn {
 
     readonly options: AwsForFluentBitAddOnProps
 
     constructor(props?: AwsForFluentBitAddOnProps) {
-        super({ ...defaultProps as any, ...props })
-        this.options = this.props
+        super({ ...defaultProps as any, ...props });
+        this.options = this.props;
     }
 
     deploy(clusterInfo: ClusterInfo): Promise<Construct> {
         const cluster = clusterInfo.cluster;
 
         // Create the FluentBit namespace.
-        const namespace = this.options.namespace
-        createNamespace(this.options.namespace!, cluster)
+        const namespace = this.options.namespace;
+        createNamespace(this.options.namespace!, cluster, true);
 
         // Create the FluentBut service account.
-        const serviceAccountName = 'aws-for-fluent-bit-sa'
+        const serviceAccountName = 'aws-for-fluent-bit-sa';
         const sa = cluster.addServiceAccount(serviceAccountName, {
             name: serviceAccountName,
             namespace: namespace
         });
 
         // Apply additional IAM policies to the service account.
-        const policies = this.options.iamPolicies || []
-        policies.forEach((policy: PolicyStatement) => sa.addToPrincipalPolicy(policy))
+        const policies = this.options.iamPolicies || [];
+        policies.forEach((policy: PolicyStatement) => sa.addToPrincipalPolicy(policy));
 
         // Configure values.
         const values = {
@@ -65,7 +68,7 @@ export class AwsForFluentBitAddOn extends HelmAddOn {
                 create: false
             },
             ...this.options.values
-        }
+        };
 
         const helmChart = this.addHelmChart(clusterInfo, values);
         return Promise.resolve(helmChart);
--- lib/addons/index.ts
+++ lib/addons/index.ts
@@ -1,45 +1,28 @@
+// EKS add-ons
+export * from './coredns';
+export * from './kube-proxy';
+export * from './vpc-cni';
 
-export * from './appmesh'
-export * from './argocd'
-export * from './aws-for-fluent-bit'
-export * from './aws-loadbalancer-controller'
-export * from './calico'
-export * from './container-insights'
-export * from './cluster-autoscaler'
-export * from './external-dns'
-export * from './metrics-server'
-export * from './nginx'
-export * from './opa-gatekeeper'
-export * from './xray'
-export * from './secrets-store'
-export * from './secrets-store/secret-provider'
-export * from './ssm-agent'
-export * from './nested-stack'
-export * from './velero'
-export * from './vpc-cni'
-export * from './coredns'
-export * from './kube-proxy'
+// Self-managed add-ons
 export * from './appmesh';
 export * from './argocd';
+export * from './aws-for-fluent-bit';
 export * from './aws-loadbalancer-controller';
+export * from './aws-node-termination-handler';
 export * from './calico';
-export * from './container-insights';
 export * from './cluster-autoscaler';
+export * from './container-insights';
 export * from './external-dns';
+export * from './karpenter';
 export * from './metrics-server';
+export * from './nested-stack';
 export * from './nginx';
 export * from './opa-gatekeeper';
-export * from './xray';
 export * from './secrets-store';
 export * from './secrets-store/secret-provider';
 export * from './ssm-agent';
-export * from './nested-stack';
 export * from './velero';
-export * from './vpc-cni';
-export * from './coredns';
-export * from './kube-proxy';
-export * from './karpenter';
-export * from './aws-node-termination-handler';
+export * from './xray';
 
 export class Constants {
     public static readonly SSP_ADDON = "ssp-addon";
